f74cb47aae44782b87121fff2b46f7c409aba1c8
adamshen <adam_ruby@126.com>
2016-12-27 20:26:19 +0800

    add topics switch button

--- src/components/top_nav.vue
+++ src/components/top_nav.vue
@@ -8,7 +8,7 @@
     <h2 class="md-title">MyRailsMap</h2>
   </md-toolbar>
 
-  <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+  <md-sidenav class="md-left" ref="leftSidenav">
     <md-toolbar class="md-large">
       <div class="md-toolbar-container">
         <div>
@@ -33,9 +33,14 @@
 
         <md-list-item>
           <md-icon>face</md-icon> <span>About</span>
-
           <md-divider class="md-inset"></md-divider>
         </md-list-item>
+
+        <div class="sidebar-switch">
+          <md-switch v-model="topics_switch_checked" id="topics_switch" name="topics_switch"  class="md-primary">
+            Topics
+          </md-switch>
+        </div>
       </md-list>
     </div>
 
@@ -45,6 +50,12 @@
 
 <script>
 export default {
+  name: 'top_nav',
+  data: function () {
+    return {
+      topics_switch_checked: false
+    }
+  },
   methods: {
     toggleLeftSidenav () {
       this.$refs.leftSidenav.toggle()
@@ -54,6 +65,10 @@ export default {
 </script>
 
 <style>
+.sidebar-switch {
+  padding: 0 16px;
+}
+
 .md-toolbar.md-large {
   min-height: 64px !important;
 }
