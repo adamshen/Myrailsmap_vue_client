ab2e7b4192d490bdf30aca1082c160e65759393c
adamshen <adam_ruby@126.com>
2017-02-04 16:41:44 +0800

    set text in home banner

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,17 @@
 <template>
 <div>
   <div class="home-banner">
+    <div class="banner-text">
+      <i class="material-icons">account_balance</i>
+    </div>
+    <div class="banner-text">KEEP</div>
+    <div class="banner-text">CALM</div>
+    <div class="banner-text">AND</div>
+    <div class="banner-text">LEARN</div>
+    <div class="banner-text">RAILS</div>
   </div>
   <div class="container">
   </div>
-</div>
 </div>
 </template>
 
@@ -35,6 +42,35 @@ export default {
 
 .home-banner {
   padding: 30px;
+  display: flex;
+  display: -webkit-flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+}
+
+.banner-text {
+  padding: 0 0.5rem;
+  line-height: 1.2;
+  font-size: 3.0rem;
+  color: rgba(0, 0, 0, 0.55);
+  text-align: center;
+}
+
+.banner-text i {
+  font-size: 4.25rem;
+}
+
+.banner-text:nth-child(2) {
+  font-size: 1.75rem;
+}
+
+.banner-text:nth-child(4) {
+  font-size: 1.5rem;
+}
+
+.banner-text:last-child {
+  font-size: 2.0rem;
 }
 
 .md-theme-blue-grey .home-banner {
