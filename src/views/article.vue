55e6685b094ef95386201ed08b8a9e7d15739761
adamshen <adam_ruby@126.com>
2017-02-23 16:16:20 +0800

    add new article button

--- src/views/article.vue
+++ src/views/article.vue
@@ -1,5 +1,10 @@
 <template>
 <div>
+  <div class="add-button">
+    <md-button class="md-fab md-mini" @click="$router.push({path: '/article/new'})">
+      <md-icon>create</md-icon>
+    </md-button>
+  </div>
   <md-tabs md-centered>
     <md-tab md-label="前端" md-icon="web">
     </md-tab>
@@ -19,3 +24,12 @@
 <script>
 export default {}
 </script>
+
+<style>
+.add-button {
+  position: absolute;
+  top: 108px;
+  right: 200px;
+  z-index: 999;
+}
+</style>
--- src/views/article/editor.vue
+++ src/views/article/editor.vue
@@ -1,10 +1,33 @@
 <template>
 <div class="editor-main">
-  <div class="">
-    <md-button :class="activeWhen('editor')" @click="switchTo('editor')">编辑</md-button>
-    <md-button :class="activeWhen('preview')" @click="switchTo('preview')">预览</md-button>
+  <h2>新建markdown文章</h2>
+  <div class="editor-buttons">
+    <md-button-toggle md-single>
+      <md-button class="md-toggle" @click="switchTo('editor')">编辑</md-button>
+      <md-button @click="switchTo('preview')">预览</md-button>
+    </md-button-toggle>
+
+    <span style="flex: 1"></span>
+    <md-button class="md-raised md-primary">提交</md-button>
   </div>
-  <textarea class="editor-input" v-model="article" v-if="currentAction === 'editor'"></textarea>
+  <div class="editor-title-input">
+    <div class="topic-selector">
+      <md-input-container>
+        <label for="article-topic">选择分类</label>
+        <md-select name="article-topic" id="article-topic" v-model="articleTopic">
+          <md-option value="font-end">前端</md-option>
+          <md-option value="back-end">后端</md-option>
+          <md-option value="coding">编程</md-option>
+          <md-option value="tools">工具控</md-option>
+        </md-select>
+      </md-input-container>
+    </div>
+    <md-input-container md-inline>
+      <label>标题</label>
+      <md-input v-model="articleTitle"></md-input>
+    </md-input-container>
+  </div>
+  <textarea class="editor-input" v-model="articleContent" v-if="currentAction === 'editor'"></textarea>
   <div id="editor-preview" v-show="currentAction === 'preview'"></div>
 </div>
 </template>
@@ -16,18 +39,17 @@ export default {
   data() {
     return {
       currentAction: 'editor',
-      article: ''
+      articleTitle: '',
+      articleContent: '',
+      articleTopic: ''
     }
   },
   methods: {
-    activeWhen(action) {
-      return action === this.currentAction ? 'md-primary' : ''
-    },
     switchTo(action) {
       this.currentAction = action
 
       if (action === 'preview') {
-        document.getElementById('editor-preview').innerHTML = marked(this.article)
+        document.getElementById('editor-preview').innerHTML = marked(this.articleContent)
 
         let blocks = this.$el.querySelectorAll('pre code')
         Array.prototype.forEach.call(blocks, window.hljs.highlightBlock)
@@ -43,17 +65,36 @@ export default {
   display: -webkit-flex;
   align-items: center;
   flex-flow: column;
-  margin-top: 4rem;
+  padding: 4rem 0;
+  width: 800px;
+  margin: auto
+}
+
+.editor-buttons {
+  display: flex;
+  display: -webkit-flex;
+  width: 100%;
+}
+
+.editor-title-input {
+  display: flex;
+  display: -webkit-flex;
+  width: 100%;
+}
+
+.topic-selector {
+  padding-right: 18px;
+  width: 150px;
 }
 
 .editor-input {
   border: 1px solid #ccc;
   border-radius: 3px;
   height: 750px;
-  width: 800px;
+  width: 100%;
 }
 
 #editor-preview {
-  width: 800px;
+  width: 100%;
 }
 </style>
