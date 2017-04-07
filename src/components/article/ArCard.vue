d873bb3a42a30d10c2dfa0ff6b19ee168092d37e
adamshen <adam_ruby@126.com>
2017-04-07 16:39:46 +0800

    show toc in ar card

--- src/components/article/ArCard.vue
+++ src/components/article/ArCard.vue
@@ -1,18 +1,33 @@
 <template>
 <div class="article-profile wrapper-center">
   <h3 v-text="article.title"></h3>
+  <p>[Toc]</p>
+  <div v-if="hasToc()">
+    <template v-for="header of article.toc.children">
+      <ar-header :header='header'></ar-header>
+    </template>
+  </div>
   <md-button class="md-primary" @click.native='readArtile'>点击阅读...</md-button>
 </div>
 </template>
 
 <script>
+import ArHeader from './ArHeader'
+
 export default {
   props: ['article'],
+  components: {
+    ArHeader
+  },
   methods: {
     readArtile() {
       this.$router.push({
         path: '/article/' + this.article.id
       })
+    },
+    hasToc() {
+      let toc = this.article.toc
+      return toc && toc.children && toc.children.length > 0
     }
   }
 }
@@ -21,5 +36,8 @@ export default {
 <style>
 .article-profile {
   min-height: 12rem;
+  width: 100%;
+  padding: 2rem 0;
+  border-top: 1px solid #607d8b;
 }
 </style>
---
+++ src/components/article/ArHeader.vue
@@ -0,0 +1,32 @@
+<template>
+<div :class='"ar-header level-" + header.level'>
+  {{ header.raw_text }}
+  <template v-if='header.children && header.children.length > 0'>
+    <template v-for='subHeader of header.children'>
+      <ar-header :header='subHeader'></ar-header>
+    </template>
+  </template>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['header'],
+  name: 'ar-header'
+}
+</script>
+
+<style>
+.ar-header {
+ line-height: 1.75;
+}
+
+.level-2 {
+  font-size: 18px;
+}
+
+.level-3 {
+  padding-left: 32px;
+  font-size: 14px;
+}
+</style>
